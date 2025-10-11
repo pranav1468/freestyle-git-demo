31c6025e56222021f26daefa7d2c0a85533d703c
pranav1468 <pranavbaghare@gmail.com>
2025-10-11 23:13:17 +0530

    demo1 updated

--- demo1.py
+++ demo1.py
@@ -2,8 +2,7 @@ print("This is my demo 1")
 for i in range(12):
     print(i,end=" ")
 
-print("hello world")
-print("heloo,how are you")
 print("changes done")
 print("hiiiiiiiiiiiiiiiii")
 print("my friend prashant")
+print("my friend is priyanshu")
